bf25ac571ce8987a253373de635046194bbb6b34
Phil Dziedzic <phil.dziedzic@gmail.com>
2015-12-05 15:11:10 -0600

    modified query again

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,6 @@ class Location < ActiveRecord::Base
   belongs_to :user
 
   	def self.search_for(query)
-		where('cast(name as text) LIKE :query OR cast(city as text) LIKE :query OR cast(state as text) LIKE :query OR cast(zip as text) LIKE :query', query: "%#{query}%")
+		where('cast(name as text) LIKE :query OR cast(city as text) LIKE :query OR cast(state as text) LIKE :query OR cast(zip as text) LIKE :query', query: "%#{cast(query as text)}%")
 	end
 end
