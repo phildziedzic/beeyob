7e4a7fa48b7af32f14fa89412911da423633dfc3
Phil Dziedzic <phil.dziedzic@gmail.com>
2015-12-05 14:58:18 -0600

    modified sql query, added doublequotes

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,6 @@ class Location < ActiveRecord::Base
   belongs_to :user
 
   	def self.search_for(query)
-		where('name LIKE :query OR city LIKE :query OR state LIKE :query OR zip LIKE :query', query: "%#{query}%")
+		where('"name" LIKE :query OR "city" LIKE :query OR "state" LIKE :query OR "zip" LIKE :query', query: "%#{query}%")
 	end
 end
